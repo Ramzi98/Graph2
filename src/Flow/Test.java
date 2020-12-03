78e3476f82fbd405aa791d16ff119d1f6bec6ae0
Tchoulak Ramzi <29214961+Ramzi98@users.noreply.github.com>
2020-12-03 20:44:06 +0100

    Debuging

--- src/Flow/Test.java
+++ src/Flow/Test.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws Exceptiongraf, IOException {
-        FlowNetwork g = new FlowNetwork("DOT\\exemple.dot");
+        FlowNetwork g = new FlowNetwork("DOT\\dg1.dot");
         int[][] mat = g.toMatrix();
         g.ford_fulkerson_execute(mat);
 
